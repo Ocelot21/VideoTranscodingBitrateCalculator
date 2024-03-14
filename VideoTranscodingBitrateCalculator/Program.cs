3a0109af5183c2aadcd9eea1cd42cfce4dcb9af0
Afan Bajraktarević <afan.bajraktarevic@edu.fit.ba>
2024-03-14 23:34:53 +0100

    File not found case

--- VideoTranscodingBitrateCalculator/Program.cs
+++ VideoTranscodingBitrateCalculator/Program.cs
@@ -5,33 +5,49 @@ string directory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecuting
 
 string jsonFilePath = Path.Combine(directory, "xvideo.json");
 
-string jsonData = File.ReadAllText(jsonFilePath);
-
-DeviceData deviceData = JsonConvert.DeserializeObject<DeviceData>(jsonData);
-
-Console.WriteLine($"Device: {deviceData.Device}");
-Console.WriteLine($"Model: {deviceData.Model}");
-
-foreach (var nic in deviceData.NIC)
+if (File.Exists(jsonFilePath))
 {
-    Console.WriteLine($"NIC Description: {nic.Description}");
-    Console.WriteLine($"MAC Address: {nic.MAC}");
-    Console.WriteLine($"Timestamp: {nic.Timestamp}");
-    Console.WriteLine($"Rx: {nic.Rx}");
-    Console.WriteLine($"Tx: {nic.Tx}");
+
+    string jsonData = File.ReadAllText(jsonFilePath);
+
+    DeviceData deviceData = JsonConvert.DeserializeObject<DeviceData>(jsonData);
+
+    Console.WriteLine($"Device: {deviceData.Device}");
+    Console.WriteLine($"Model: {deviceData.Model}");
+    Console.WriteLine();
+    Console.WriteLine($"Network interfaces list:");
+    Console.WriteLine();
+    foreach (var nic in deviceData.NIC)
+    {
+        Console.WriteLine($"NIC Description: {nic.Description}");
+        Console.WriteLine($"MAC Address: {nic.MAC}");
+        Console.WriteLine($"Timestamp: {nic.Timestamp}");
+        Console.WriteLine($"Rx: {nic.Rx}");
+        Console.WriteLine($"Tx: {nic.Tx}");
+        Console.WriteLine("_____________________________________");
+    }
+
+
+    Console.WriteLine();
+    Console.WriteLine("Bitrate for each NIC (kbps):");
+    foreach (var nic in deviceData.NIC)
+    {
+        Console.WriteLine();
+        double rxKb = (nic.Rx * 8) / 1000.0;
+        double txKb = (nic.Tx * 8) / 1000.0;
+
+        double time = 1f / Settings.PullingRate; // Time in seconds - One second is divded by pulling rate (Hz) 
+
+        double rxBitrate = rxKb / time;
+        double txBitrate = txKb / time;
+
+        Console.WriteLine($"  Rx Bitrate: {rxBitrate:N2} kbps");
+        Console.WriteLine($"  Tx Bitrate: {txBitrate:N2} kbps");
+        Console.WriteLine("_____________________________________");
+    }
 }
 
-Console.WriteLine("Bitrate for each NIC (kbps):");
-foreach (var nic in deviceData.NIC)
+else
 {
-    double rxKbps = (nic.Rx * 8) / 1000.0;
-    double txKbps = (nic.Tx * 8) / 1000.0;
-
-    double time = 1f / Settings.PullingRate; // Time in seconds
-
-    double rxBitrateKbps = rxKbps / time;
-    double txBitrateKbps = txKbps / time;
-
-    Console.WriteLine($"  Rx Bitrate: {rxBitrateKbps:N2} kbps");
-    Console.WriteLine($"  Tx Bitrate: {txBitrateKbps:N2} kbps");
+    Console.WriteLine("ERROR: File not found");
 }
